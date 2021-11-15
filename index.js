8132e90c9f8445c4c075faf42213761ef5eafa55
mirjade2008 <mirjade2008@yahoo.com>
2021-11-15 07:59:37 -0800

    forLoopHW

---
+++ index.js
@@ -0,0 +1,32 @@
+const  myArray = [];
+
+for(var i = 1; i < 10; i++);
+myArray.push(i);
+
+console.log (myArray);
+
+
+
+var i = 1;
+while(i < 10) { 
+myArray.push (i);
+i++;
+}
+
+console.log (myArray);
+
+
+
+ 
+for(var i = 11; i < 22; i+=2) {
+    myArray.push (i);
+}
+
+console.log (myArray);
+
+
+for(var i = 15; i > 0; i-=2){
+    myArray.push(i);
+}
+
+console.log (myArray);
